470ad02c7121b45208cb5cf3239747884bc134e9
yuanj1ng <2582076326@qq.com>
2025-06-24 11:54:27 +0800

    计算距离使用sqrt

--- P1429.cpp
+++ P1429.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <algorithm>
 #include <iomanip>
+#include <limits>
 
 struct Point{
     double x;
@@ -10,26 +11,19 @@ struct Point{
 };
 
 double cal_dist(Point a, Point b){
-    return pow((a.x-b.x),2) + pow((a.y-b.y),2);
+    return sqrt(pow((a.x-b.x),2) + pow((a.y-b.y),2));
 }
 
-bool cmp_x(Point a, Point b){
+bool cmp(Point a, Point b){
     return a.x < b.x;
-}
-
-bool cmp_y(Point a, Point b){
-    return a.y < b.y;
 }
 
 double find_min_dist(const std::vector<Point> &point_v,int l_ind,int r_ind){
     double min_dist = std::numeric_limits<double>::max();
-    if(r_ind-l_ind <=3){ 
-        for(int i = l_ind;i<=r_ind;i++){
-            for(int j=i+1;j<=r_ind;j++){
-                double temp = cal_dist(point_v[i],point_v[j]);
-                if(temp<min_dist){
-                    min_dist = temp;
-                }
+    if(r_ind-l_ind <=3){
+        for(int i=l_ind;i<=r_ind;++i){
+            for(int j=i+1;j<=r_ind;++j){
+                min_dist = std::min(cal_dist(point_v[i],point_v[j]),min_dist);
             }
         }
         return min_dist;
@@ -39,20 +33,18 @@ double find_min_dist(const std::vector<Point> &point_v,int l_ind,int r_ind){
     double l_dist = find_min_dist(point_v,l_ind,mid);
     double r_dist = find_min_dist(point_v,mid+1,r_ind);
     min_dist = std::min(l_dist,r_dist);
-    std::vector<Point> strip_points;
-    for(int i = l_ind;i<=r_ind;i++){
-        if(pow(point_v[i].x - mid_x,2) < min_dist){
-            strip_points.push_back(point_v[i]);
+    std::vector<Point> strip_point;
+    for(int i=l_ind;i<=r_ind;++i){
+        if(std::abs(point_v[i].x-mid_x)<min_dist){
+            strip_point.push_back(point_v[i]);
         }
     }
-    std::sort(strip_points.begin(), strip_points.end(),cmp_y);
-    for(size_t i=0;i<strip_points.size();i++){
-        for(size_t j=i+1;j<strip_points.size();j++){
-            if (pow(strip_points[j].y - strip_points[i].y, 2) >= min_dist) {
+    for(size_t i=0;i<strip_point.size();++i){
+        for(size_t j=i+1;j<strip_point.size();++j){
+        if(std::abs(strip_point[i].y-strip_point[j].y)>min_dist){
             break;
         }
-        // 更新最小距离
-        min_dist = std::min(min_dist, cal_dist(strip_points[i], strip_points[j]));   
+        min_dist = std::min(cal_dist(point_v[i],point_v[j]),min_dist);
         }
     }
     return min_dist;
@@ -63,12 +55,12 @@ int main(){
     std::cin >> n;
     std::vector<Point> point_v(n);
     for(int i =0;i<n;i++){
-    Point point;
-    std::cin >> point.x >> point.y;
-    point_v[i] = point;
+        Point point;
+        std::cin >> point.x >> point.y;
+        point_v[i] = point;
     }
-    std::sort(point_v.begin(),point_v.end(),cmp_x);
+    std::sort(point_v.begin(),point_v.end(),cmp);
     double ans = find_min_dist(point_v,0,n-1);
-    std::cout << std::fixed << std::setprecision(4) << sqrt(ans) << std::endl;
+    std::cout << std::fixed << std::setprecision(4) << ans << std::endl;
     return 0;
 }
---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, VSCode + g++ + WSL!" << std::endl;
+    return 0;
+}
